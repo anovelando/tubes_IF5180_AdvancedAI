c880d166fd0aa42eb52f6bd36452ef51ada5ddea
andrenovelando <andre.novelando@kemenkeu.go.id>
2023-11-22 21:06:28 +0700

    Add knowledge-based agent

---
+++ src/main_test.py
@@ -0,0 +1,58 @@
+import random
+
+from board import *
+from gold import *
+from player import *
+from wumpus import *
+from movement import *
+from pit import *
+
+def main(board_size = 10):
+    # Print header
+    print("⭐⭐WELCOME TO IF5080 SIMPLE WUMPUS WORLD⭐⭐")
+
+    # Set board and player
+    # board_size = 10
+    myBoard = Board(board_size)
+    myPlayer = Player()
+
+    # create wumpuses and pits
+    listWumpus = []
+    listPit = []
+    enemyPoint = []
+    while len(listWumpus) < 3:
+        point = [random.randint(2, board_size - 1), random.randint(2, board_size - 1)]
+        if point not in enemyPoint:
+            enemyPoint.append(point)
+            wumpus = Wumpus(point[0], point[1])
+            listWumpus.append(wumpus)
+    while len(listPit) < 3:
+        point = [random.randint(2, board_size - 1), random.randint(2, board_size - 1)]
+        if point not in enemyPoint:
+            enemyPoint.append(point)
+            pit = Pit(point[0], point[1])
+            listPit.append(pit)
+
+    #create gold
+    while True:
+        goldPoint = [random.randint(7, board_size - 1), random.randint(4, board_size - 1)]
+        if goldPoint not in enemyPoint:
+            gold = Gold(goldPoint[0], goldPoint[1])
+            break
+
+    myBoard.init_board(myPlayer, listWumpus, listPit, gold)
+
+    while True:
+        print()
+        myBoard.print_board()
+        print("Number of player moves:", myPlayer.num_movement)
+        print("Player perception:", myPlayer.percept)
+        dir = movement(myPlayer)
+        myPlayer.num_movement += 1
+        myPlayer.move(myBoard, dir)
+
+        if myPlayer.is_finished(listWumpus, listPit, gold):
+            break
+        
+for i in range(100):
+    main(random.randint(10,20))
--- src/player.py
+++ src/player.py
@@ -10,6 +10,20 @@ class Player(object):
             "breeze": False,
         }
 
+        # Added Attributes
+        self.visited = {}
+        self.wall = []
+        self.safe = []
+        self.threat = []
+        self.pit = []
+        self.wumpus = []
+        self.path = []
+
+        self.prev_x = None
+        self.prev_y = None
+        self.safe.append((self.x, self.y))
+        self.last_move = None
+
     def move(self, board, direction):
         board.update_board(self.x, self.y, board.board_static[self.x][self.y])
 
@@ -40,3 +54,153 @@ class Player(object):
             print("======================\nCongratulations, you win 😄")
             return True
         return False
+    
+    # Added methods
+
+    def update_knowledge(self):
+        cur_pos = (self.x, self.y)
+        prev_pos = (self.prev_x, self.prev_y)
+
+        # Delete current position from unvisited list
+        if cur_pos in self.safe:
+            self.safe.remove(cur_pos)
+        if cur_pos in self.threat:
+            self.threat.remove(cur_pos)
+
+        # Hit the wall
+        if cur_pos == prev_pos:
+            if self.last_move not in self.wall:
+                self.wall.append(self.last_move)
+            if self.last_move in self.safe:
+                self.safe.remove(self.last_move)
+            if self.last_move in self.threat:
+                self.threat.remove(self.last_move)
+
+        # Percept
+        self.visited[cur_pos] = self.percept
+
+        # Update adjacent rooms
+        adjs = self.get_adjacent(cur_pos)
+        if not self.percept['stench'] and not self.percept['breeze']:
+            for p in adjs:
+                if p not in self.safe:
+                    self.safe.append(p)    
+        else:
+            for p in adjs:
+                self.safe_threat(p)
+
+        # Find threats
+        self.find_threat()
+
+    def get_adjacent(self, position):
+        x, y = position
+        adj = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        adj = list(filter(lambda p: p not in self.visited and p not in self.wall and p not in self.wumpus and p not in self.pit, adj))
+        return adj
+    
+    def safe_threat(self, position):
+        x, y = position
+        adj = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        adj = list(filter(lambda p: p in self.visited, adj))
+        
+        for i in range(len(adj)):
+            i_percept = (self.visited[adj[i]]['stench'], self.visited[adj[i]]['breeze'])
+            if i_percept == (True, True):
+                continue
+            for j in range(i+1,len(adj)):
+                j_percept = (self.visited[adj[j]]['stench'], self.visited[adj[j]]['breeze'])
+                if j_percept == (True, True):
+                    continue
+                if i_percept != j_percept:
+                    if position not in self.safe:
+                        self.safe.append(position)
+                    if position in self.threat:
+                        self.threat.remove(position)
+                    return
+                
+        if position not in self.threat:
+            self.threat.append(position)
+    
+    def find_threat(self):
+        x, y = self.x, self.y
+        percept = (self.visited[(x, y)]['stench'], self.visited[(x, y)]['breeze'])
+        if percept == (False, False):
+            return
+        adj = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        potential_threat = list(filter(lambda p: p not in self.visited and p not in self.safe and p not in self.wall, adj))
+        wumpus = list(filter(lambda p: p in self.wumpus, adj))
+        pit = list(filter(lambda p: p in self.pit, adj))
+        if len(potential_threat) == 1:
+            if percept[0] and len(wumpus) == 0:
+                self.wumpus.append(potential_threat[0])
+                self.threat.remove(potential_threat[0])
+                return
+            if percept[1] and len(pit) == 0:
+                self.pit.append(potential_threat[0])
+                self.threat.remove(potential_threat[0])
+                return
+        
+        if len(potential_threat) == 2 and percept == (True, True) and (len(wumpus) == 0 or len(pit) == 0):
+            self.wumpus += potential_threat
+            self.pit += potential_threat
+            for p in potential_threat:
+                if p in self.threat:
+                    self.threat.remove(p)
+
+            
+    def forward(self):
+        cur_pos = (self.x, self.y)
+        if len(self.path) == 0:
+            if len(self.safe):
+                goal_position = self.safe[-1]
+                self.path = self.dfs(cur_pos, goal_position, [])
+            elif len(self.threat):
+                goal_position = self.threat[-1]
+                self.path = self.dfs(cur_pos, goal_position, [])
+            else:
+                import sys
+                sys.exit("No solution")
+
+        next_pos = self.path.pop(0)
+        self.prev_x, self.prev_y = cur_pos
+        self.last_move = next_pos
+
+        return self.forward_command(cur_pos, next_pos)
+
+
+    # Map point to command based on move function
+    def forward_command(self, cur_pos, next_pos):
+        x, y = cur_pos
+        dx, dy = next_pos
+        move = (dx-x, dy-y)
+        if move == (0, 1):
+            return 'D'
+        if move == (0, -1):
+            return 'A'
+        if move == (1, 0):
+            return 'S'
+        if move == (-1, 0):
+            return 'W'
+            
+    def dfs(self, cur_pos, goal_position, visited=[]):
+        visited.append(cur_pos)
+        
+        x, y = cur_pos
+        dx, dy = goal_position
+        s = (abs(dx - x), abs(dy - y))
+        if s in [(1,0), (0,1)]:
+            return [goal_position]
+        
+        adj = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        adj = list(filter(lambda p: p in self.visited and p not in visited, adj))
+
+        if len(adj) == 0:
+            return None
+        
+        for p in adj:
+            path = self.dfs(p, goal_position, visited)
+            if path:
+                return [p] + path
+            
+        return None
+                
